591d1a38e31ce010c5aceaaac72664c00a474bbb
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-08-13 19:05:35 +0700

    add

--- fweb_final/src/App.js
+++ fweb_final/src/App.js
@@ -1,4 +1,4 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Footer from './Footer';
 import ForumPage from './ForumPage';
 import Navbar from './Navbar';

ed2c349c2c3b8a6b039d550bd83a2666057e6ff3
Giang Le Quynh <s3802828@rmit.edu.vn>
2021-08-14 02:11:11 +0700

    routing

--- fweb_final/src/App.js
+++ fweb_final/src/App.js
@@ -7,18 +7,30 @@ import NewsPage from './NewsPage';
 import Login from './Login';
 import Signup from './Signup';
 import Posts from './Posts';
+import ReportPage from './ReportPage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
   return (
     <div>
-
-      <Navbar />
-      <NewsPage />
+      {/*<NewsPage />*/}
       {/*<CreatePost />*/}
       {/*<ForumPage />*/}
-      {/* <Footer /> */}
-      {/*<Login/>*/}
-      {/*<Signup/>*/}
-      <Posts />
+      {/**/}
+      {/**/}
+      {/*<Posts />*/}
+      {/*<ReportPage />*/}
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact base path="/"><NewsPage /></Route>
+          <Route path="/forum"><ForumPage /></Route>
+          <Route path="/login"><Login/></Route>
+          <Route path="/signup"><Signup/></Route>
+          <Route path="/articles"><ReportPage /></Route>
+        </Switch>
+        <Footer />
+      </Router>
+     
     </div>
   );
 }

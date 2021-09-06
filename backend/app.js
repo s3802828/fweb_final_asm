ca51b449a62ba15dd1e6bb6059f52d30c588f251
Nguyen Ha Kieu Anh <s3818552@rmit.edu.vn>
2021-09-06 21:40:27 +0700

    delete

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ mongoose.connect("mongodb+srv://giangle:mypassword@cluster0.sfxdv.mongodb.net/fu
 .then(() => app.listen(9000, () => console.log("Succesfully connected!!!")))
 .catch((error) => console.log(error.message))
 
-
 var postCategoryJSON = require("./samplePostCategory.json")
 var Post_category = require("./models/post_category")
 Post_category.post_category.insertMany(postCategoryJSON, function(error, data){

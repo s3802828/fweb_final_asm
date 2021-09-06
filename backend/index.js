ec85a555d6bcfed6e6e7acdff5aa6c98fcd3c803
s3802828 <56726405+s3802828@users.noreply.github.com>
2021-09-06 23:05:52 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
+const mongoose = require('mongoose')
+const { MONGO_URI } = process.env;
 
 const { SERVER_PORT } = process.env;
+mongoose.connect(MONGO_URI)
 
-// server listening 
-server.listen(SERVER_PORT, () => {
-  console.log(`Server running on port ${SERVER_PORT}`);
-});
+  .then(() => server.listen(SERVER_PORT, () => {
+    console.log(`Server running on port ${SERVER_PORT}. Database successfully connected`);
+  }))
+  .catch((error) => console.log(error.message))
+
